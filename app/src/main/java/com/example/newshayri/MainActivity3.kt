e116062eb031d3354641fe09320aac5e5e36607a
kaushikk26 <kaushiksardhara91@gmail.com>
2024-01-31 17:32:40 +0530

    set

--- app/src/main/java/com/example/newshayri/MainActivity3.kt
+++ app/src/main/java/com/example/newshayri/MainActivity3.kt
@@ -15,27 +15,18 @@ import androidx.viewpager.widget.ViewPager
 class MainActivity3 : AppCompatActivity() {
 
     lateinit var done: ViewPager
-    lateinit var leftk: Button
-    lateinit var rightk: Button
     var set2 = arrayOf<String>("")
-    lateinit var copy: ImageView
-    lateinit var whatsapp: ImageView
-    lateinit var share: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
 
         done = findViewById(R.id.done)
-//        copy = findViewById(R.id.copy)
-//        share = findViewById(R.id.share)
-//        whatsapp = findViewById(R.id.whatshap)
+
 
         var position = intent.getIntExtra("pos", 0)
 
         var set = intent.getStringExtra("m2")
-
-//        done.setText(set)
     
         set2 = intent.getStringArrayExtra("array")!!
 
@@ -44,53 +35,5 @@ class MainActivity3 : AppCompatActivity() {
 
         done.setCurrentItem(position)
 
-//        copy.setOnClickListener {
-//
-//            val textToCopy = done.toString()
-//            val clipboardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-//            val clipData = ClipData.newPlainText("text", textToCopy)
-//            clipboardManager.setPrimaryClip(clipData)
-//            Toast.makeText(this, "Text copied to clipboard", Toast.LENGTH_LONG).show()
-//
-//            Toast.makeText(this,"copy",Toast.LENGTH_SHORT).show()
-//        }
-//
-//        share.setOnClickListener {
-//            val sendIntent: Intent = Intent().apply {
-//                action = Intent.ACTION_SEND
-//                putExtra(Intent.EXTRA_TEXT, done.toString())
-//                type = "text/plain"
-//            }
-//
-//            val shareIntent = Intent.createChooser(sendIntent, null)
-//            startActivity(shareIntent)
-//        }
-//
-//        whatsapp.setOnClickListener {
-//            val messageEditText = done.toString()
-//            val message = messageEditText.toString()
-//
-//
-//            sendMessage(message);
-//        }
-//    }
-//    private fun sendMessage(message: String) {
-//
-//        val intent = Intent(Intent.ACTION_SEND.toString())
-//
-//        intent.type = "text/plain"
-//
-//        intent.setPackage("com.whatsapp")
-//
-//        intent.putExtra(Intent.EXTRA_TEXT.toString(), message)
-//
-//        if (intent.resolveActivity(packageManager) == null) {
-//            Toast.makeText(this,
-//                "Please install whatsapp first.",
-//                Toast.LENGTH_SHORT).show()
-//            return
-//        }
-//        startActivity(intent)
-//    }
     }
 }
